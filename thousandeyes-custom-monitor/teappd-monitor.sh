5cc6b6d5e104b0f63e5d0962300f36e7ae45e42a
David <davidryder@yahoo.com>
2020-08-17 16:02:46 -0700

    TE Added

---
+++ thousandeyes-custom-monitor/teappd-monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Convention: testname = <application>-<tier>-<node>
+# Example: adcapital-frontend-frontend1
+# ./teappd-monitor.py "<test name>" <TE account email> <TE API token> <test name>
+./teappd-monitor.py "Lab" alex.kozlov@wwt.com q2rwmlev28e8igzlahdx3wg0fkskrl7i "TeaStore Order - PageLoad"
