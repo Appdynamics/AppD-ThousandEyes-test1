9d2e8e072e61a1c3916a69f75ec358e0b9d25dd3
David <davidryder@yahoo.com>
2020-08-18 09:45:27 -0700

    Updates

--- thousandeyes-custom-monitor/teappd-monitor.sh
+++ thousandeyes-custom-monitor/teappd-monitor.sh
@@ -2,4 +2,4 @@
 # Convention: testname = <application>-<tier>-<node>
 # Example: adcapital-frontend-frontend1
 # ./teappd-monitor.py "<test name>" <TE account email> <TE API token> <test name>
-./teappd-monitor.py "Lab" alex.kozlov@wwt.com q2rwmlev28e8igzlahdx3wg0fkskrl7i "TeaStore Order - PageLoad"
+./teappd-monitor.py "Lab" $THOUSANDEYES_USER $THOUSANDEYES_API_TOKEN "TeaStore Order - PageLoad"
