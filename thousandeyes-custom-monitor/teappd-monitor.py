80dbd5bbabeec0972490863a5b47ae70bd476459
David <davidryder@yahoo.com>
2020-08-18 09:46:43 -0700

    Updates - debug statements

--- thousandeyes-custom-monitor/teappd-monitor.py
+++ thousandeyes-custom-monitor/teappd-monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # teappd-monitor.py - a Python script for querying ThousandEyes test metrics, transforming metrics JSON payload, and pushing to AppD.
-# 
+#
 
 import os, time, sys, base64, json,requests
 from datetime import datetime
@@ -29,9 +29,9 @@ class TeApi:
         request_headers = self.getRequestHeaders()
         params = self.te_params
 
-        #print ("API: " + api_url)
+        print ("API: " + api_url)
         api_response = requests.get(api_url, headers=request_headers) if params is None else requests.get(api_url, headers=request_headers, params=params)
-        #print (api_response)
+        print ("API Response ", api_response)
         return api_response.json()
 
     def getAccounts (self):
@@ -40,14 +40,14 @@ class TeApi:
     def getAccountId (self, accountName):
         accounts = self.getAccounts()['accountGroups']
         return next((a['aid'] for a in accounts if a['accountGroupName'] == accountName), None)
-        
+
     def getTests (self):
         return self.get('tests')
 
-    def getTestDetails (self, testId): 
+    def getTestDetails (self, testId):
         return self.get('tests/' + testId)
 
-    def getAgentDetails (self, agentId): 
+    def getAgentDetails (self, agentId):
         return self.get('agents/' + agentId)
 
     def getTestNetData(self, testId):
@@ -55,10 +55,10 @@ class TeApi:
 
     def getTestBgpData(self, testId):
         return self.get('net/bgp-metrics/' + str(int(testId)))
-        
+
     def getTestPageloadData(self, testId):
         return self.get('web/page-load/' + str(int(testId)))
-    
+
     def getTestHttpData (self, testId) :
         return self.get('web/http-server/' + str(int(testId)))
 
@@ -70,13 +70,13 @@ class TeApi:
 
 # Helper function for updating the aggregated JSON metrics data
 def update_dict (dictionary, key, data):
-    if key in dictionary : 
+    if key in dictionary :
         if type(dictionary[key]) is list : dictionary[key].extend(data)
         else : dictionary[key].update(data)
     else :
         dictionary[key] = data
 
-# Query agent details, including agent label. In addition, if label name is the form of a JSON element ("<tag>: <value>") 
+# Query agent details, including agent label. In addition, if label name is the form of a JSON element ("<tag>: <value>")
 # then convert the label to a <key>:<value> tag as well. Passed agenddata object is updated with agent details and label/tag info.
 def query_agent_details (teApi, agentdata):
     agentdata.update (teApi.getAgentDetails (str(agentdata['agentId']))['agents'][0])
@@ -85,8 +85,8 @@ def query_agent_details (teApi, agentdata):
         agentdata['tags'].update({ group['name'].split(': ')[0] : group['name'].split(': ')[1] }) if ':' in group['name']  else agentdata['tags'].update({ group['name'] : group['name'] })
 
 
-    # "application": "string", 
-    # "tier": "string", 
+    # "application": "string",
+    # "tier": "string",
     # Convention: testname = <application>-<tier>
     # Example: adcapital-frontend
 
@@ -104,7 +104,7 @@ def query_latest_data(username, authtoken, accountname, testname):
         # for group in (group for group in testDetails['groups'] if group['groupId'] > 0):
         #     testDetails['tags'].update({ group['name'].split(': ')[0] : group['name'].split(': ')[1] }) if ':' in group['name']  else testDetails['tags'].update({ group['name'] : group['name'] })
         appinfo=testname.split('-')
-        if (len(appinfo) < 3) : 
+        if (len(appinfo) < 3) :
             appinfo.extend((3-len(appinfo)) * [None])
 
         for agentdata in testDetails['agents']:
@@ -135,9 +135,9 @@ def query_latest_data(username, authtoken, accountname, testname):
 
             # ensure these metrics are always present in the JSON object:
 
-            update_dict(aggdata, key, {'connectTime':''}) 
+            update_dict(aggdata, key, {'connectTime':''})
             update_dict(aggdata, key, {'errorDetails':''})
-            update_dict(aggdata, key, {'receiveTime':''}) 
+            update_dict(aggdata, key, {'receiveTime':''})
             update_dict(aggdata, key, {'responseTime':''})
             #update_dict(aggdata, key, {'throughput':''})
 
@@ -145,7 +145,7 @@ def query_latest_data(username, authtoken, accountname, testname):
                 testdata = teApi.getTestPageloadData(test['testId'])
                 if testdata :
                     for agentdata1 in testdata['web']['pageLoad'] :
-                        key = testname + " - " + agentdata1['agentName'] 
+                        key = testname + " - " + agentdata1['agentName']
                         update_dict(aggdata, key, agentdata1)
                         update_dict(aggdata, key, {'target':testdata['web']['test']['url']})
 
@@ -153,15 +153,15 @@ def query_latest_data(username, authtoken, accountname, testname):
                 httpdata = teApi.getTestHttpData(test['testId'])
                 if httpdata :
                     for agentdata2 in httpdata['web']['httpServer'] :
-                        key = testname + " - " + agentdata2['agentName'] 
+                        key = testname + " - " + agentdata2['agentName']
                         update_dict(aggdata, key, agentdata2)
 
             if (test['type'] == 'page-load') or (test['type'] == 'web-transactions') or (test['type'] == 'http-server') or (test['type'] == 'agent-to-server') :
                 networkdata = teApi.getTestNetData(test['testId'])
                 if networkdata:
                     for agentdata3 in networkdata['net']['metrics'] :
-                        key = testname + " - " + agentdata3['agentName'] 
-                        # Convert date time to ISO 
+                        key = testname + " - " + agentdata3['agentName']
+                        # Convert date time to ISO
                         agentdata3['date'] = datetime.strptime (agentdata3['date'], '%Y-%m-%d %H:%M:%S').isoformat()
                         update_dict(aggdata, key, agentdata3)
             else :
@@ -172,8 +172,8 @@ def query_latest_data(username, authtoken, accountname, testname):
     except Exception as e:
         print (json.dumps({"error": "Could not load test {} from account {}. (Exception: {})".format(testname, accountname, e)}))
         #print (json.dumps())
-        
-    
+
+
     #print (json.dumps(aggdata))
     return aggdata
 
@@ -181,7 +181,7 @@ def query_latest_data(username, authtoken, accountname, testname):
 if __name__ == '__main__':
     if len(sys.argv) < 5:
         print (json.dumps({"Usage": "te-monitor <account name> <login email> <api token> <testname 1> <testname N>"}))
-    
+
     accountgroup = sys.argv[1]
     username = sys.argv[2]
     authtoken = sys.argv[3]
@@ -204,6 +204,10 @@ if __name__ == '__main__':
     with open('connection.json') as f:
         connectionInfo = json.loads(f.read())
         try :
+            print('curl -s -X POST "' + connectionInfo['analytics-api'] + '/events/schema/' + schemaname + '" \
+            -H"X-Events-API-AccountName:' + connectionInfo['account-id'] + '" \
+            -H"X-Events-API-Key:' + connectionInfo['api-key'] + '" -H"Content-type: application/vnd.appd.events+json;v=2" \
+            -d \'{"schema" : ' + json.dumps(schema) + '} \' &>/dev/null')
             os.system('curl -s -X POST "' + connectionInfo['analytics-api'] + '/events/schema/' + schemaname + '" \
             -H"X-Events-API-AccountName:' + connectionInfo['account-id'] + '" \
             -H"X-Events-API-Key:' + connectionInfo['api-key'] + '" -H"Content-type: application/vnd.appd.events+json;v=2" \
@@ -212,30 +216,30 @@ if __name__ == '__main__':
         # PATCH http://analytics.api.example.com/events/schema/{schemaName}
         # X-Events-API-AccountName:<global_account_name>
         # X-Events-API-Key:<api_key>
-        except :
-            print ()
+        except Exception as e:
+            print (e)
 
     for test in tests :
         # aggregate test data across all tests as an array of flattened test metric objects
         testdata.extend(list((query_latest_data (username, authtoken, accountgroup, test)).values()))
-    
+
     print ("\n\n")
     #print (json.dumps(testdata))
     for testround in testdata :
         for metric in metrics :
             if metric in testround and testround[metric] != "":
                 print ("name=Custom Metrics|{0}|{1}|{2}, value={3}".format(testround['testName'], testround['agentName'].replace(',', ' '), metrics[metric], testround[metric]))
-    #      print(json.dumps(testround))
-    #     #jsondata = json.dumps(testround)
-    #     print (testround['date'])
-    #     print(json.dumps(testround))
-    #     post = "curl -s -X POST \"{0}/events/publish/{1}\" -H\"X-Events-API-AccountName: {2}\" -H\"X-Events-API-Key: {3}\" -H\"Content-type: application/vnd.appd.events+json;v=2\" -d \'[{4}]\'".format(
-    #         connectionInfo['analytics-api'],
-    #         schemaname, 
-    #         connectionInfo['account-id'],
-    #         connectionInfo['api-key'],
-    #         json.dumps(testround))
-    #     os.system (post)
-        #print (json.dumps(metric)) #post = "curl -s -X POST -H \"User-Agent: ThousandEyes\" {} -d \'{}\'".format(targeturl, json.dumps(metric))
-        #os.system (post)
-
+        print(json.dumps(testround))
+        #jsondata = json.dumps(testround)
+        print ("DATE", testround['date'])
+        print("DATA", json.dumps(testround))
+        post = "curl -s -X POST \"{0}/events/publish/{1}\" -H\"X-Events-API-AccountName: {2}\" -H\"X-Events-API-Key: {3}\" -H\"Content-type: application/vnd.appd.events+json;v=2\" -d \'[{4}]\'".format(
+            connectionInfo['analytics-api'],
+            schemaname,
+            connectionInfo['account-id'],
+            connectionInfo['api-key'],
+            json.dumps(testround))
+        print( "POST", post )
+        os.system (post)
+        print (json.dumps(metric)) #post = "curl -s -X POST -H \"User-Agent: ThousandEyes\" {} -d \'{}\'".format(targeturl, json.dumps(metric))
+        os.system (post)
